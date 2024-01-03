c8243a5549b63d4821addee4132e539908dfb77b
BirdieOA <maitedeleonf@gmail.com>
2024-01-03 10:09:52 -0300

    actividad-2

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import "./Micss.css";
 import imagen1 from "./imagenes/rey_atanagildo.png";
 import imagen2 from "./imagenes/rey_ataulfo.png";
 import imagen3 from "./imagenes/rey_ervigio.png";
+import rey from "./imagenes/rey_incognito.png";
 
 function App() {
   let nombres = ["Atanagildo", "Ataulfo", "Ervigio"];
+const cambiarImagen = (e) =>{
+  if(e.target.src.includes("incognito")){
+    e.target.style.visibility="hidden";
+  } else{
+    e.target.src=rey;
+  }
+  e.target.parentNode.style.backgroundColor="white";
+}
+
+  const cambiarTexto = (e) => {
+    if(e.target.innerHTML == "Visto"){
+      e.target.innerHTML = "";
+    } else {
+      e.target.innerHTML="Visto";
+    }
+  };
   return (
     <div className="container">
       <div className="caja">
-        {nombres[0]}
-        <img src={imagen1} className="img" />
+        <img onClick={cambiarImagen} src={imagen1} />
+        <div onClick={cambiarTexto} className="nombre">
+          {nombres[0]}
+        </div>
       </div>
       <div className="caja">
-        {nombres[1]}
-        <img src={imagen2} className="img" />
+        <img onClick={cambiarImagen} src={imagen2} />
+        <div onClick={cambiarTexto} className="nombre">
+          {nombres[1]}
+        </div>
       </div>
       <div className="caja">
-        {nombres[2]}
-        <img src={imagen3} className="img" />
+        <img onClick={cambiarImagen} src={imagen3} />
+        <div onClick={cambiarTexto} className="nombre">
+          {nombres[2]}
+        </div>
       </div>
     </div>
   );
